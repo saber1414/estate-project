f76a4a00b17a46cb0fe8599c98b6560054c75ceb
Saber___Dev <saber.esmaili1414@gmail.com>
2025-05-31 13:58:31 +0330

    feat: add new project estate

---
+++ pages/homes/[id].jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleHomeDetailsPage = () => {
+  return (
+    <>
+        <h1>Single Home Details Page 😎</h1> 
+    </>
+  )
+}
+
+export default SingleHomeDetailsPage
---
+++ pages/homes/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+        <h1>All Homes Page</h1> 
+    </>
+  )
+}
+
+export default Index
