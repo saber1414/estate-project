222b0d272ca6cae2471278efed9dbed82f242511
Saber___Dev <saber.esmaili1414@gmail.com>
2025-05-31 14:15:39 +0330

    feat: update component feautr

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import Sidebar from "@/components/modules/sidebar/sidebar";
+import Header from "@/components/modules/header/header";
+import RealTors from "@/components/modules/realTors/realTors";
+import Footer from "@/components/modules/footer/footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container">
+      <Sidebar />
+      <Header />
+      <RealTors />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
