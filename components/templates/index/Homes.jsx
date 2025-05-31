21508934a561104196930922c095f528c8f27479
Saber___Dev <saber.esmaili1414@gmail.com>
2025-05-31 18:49:32 +0330

    feat: update db.json - add home component

--- components/templates/index/Homes.jsx
+++ components/templates/index/Homes.jsx
@@ -1,183 +1,25 @@
-import React from "react";
+import Home from "@/components/modules/Home";
+import React, { useEffect, useState } from "react";
+import data from "../../../data/db.json";
 
 const Homes = () => {
+  const [homes, setHomes] = useState([]);
+  
+  const randomHomes = (arr,randomItem) => {
+    let shuffled = [...arr].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0,randomItem)
+  };
+
+  useEffect(() => {
+    setHomes(data.homes)
+  }, [homes]);
+
   return (
     <>
       <div className="homes">
-        <div className="card">
-          <img src="img/gal-1.jpeg" alt="House 6" className="card__img" />
-          <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
-          <span className="card__like">
-            <i className="fa fa-heart"></i>
-          </span>
-          <div className="card__details">
-            <span className="">
-              <i className="fa fa-map-marker card__icon"></i>
-            </span>
-            <p className="card__text">مالدیو</p>
-            <span className="">
-              <i className="fa fa-user card__icon"></i>
-            </span>
-            <p className="card__text">4 اتاق</p>
-            <span className="">
-              <i className="fa fa-expand card__icon"></i>
-            </span>
-            <p className="card__text">400 متر مربع</p>
-            <span className="">
-              <i className="fa fa-key card__icon"></i>
-            </span>
-            <p className="card__text">560 میلیون تومان</p>
-          </div>
-
-          <a href="#" className="btn btn-brown btn-card">
-            مشاهده ملک
-          </a>
-        </div>
-        <div className="card">
-          <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
-          <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
-          <span className="card__like">
-            <i className="fa fa-heart"></i>
-          </span>
-          <div className="card__details">
-            <span className="">
-              <i className="fa fa-map-marker card__icon"></i>
-            </span>
-            <p className="card__text">مالدیو</p>
-            <span className="">
-              <i className="fa fa-user card__icon"></i>
-            </span>
-            <p className="card__text">4 اتاق</p>
-            <span className="">
-              <i className="fa fa-expand card__icon"></i>
-            </span>
-            <p className="card__text">400 متر مربع</p>
-            <span className="">
-              <i className="fa fa-key card__icon"></i>
-            </span>
-            <p className="card__text">560 میلیون تومان</p>
-          </div>
-
-          <a href="#" className="btn btn-brown btn-card">
-            مشاهده ملک
-          </a>
-        </div>
-        <div className="card">
-          <img src="img/gal-3.jpeg" alt="House 6" className="card__img" />
-          <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
-          <span className="card__like">
-            <i className="fa fa-heart"></i>
-          </span>
-          <div className="card__details">
-            <span className="">
-              <i className="fa fa-map-marker card__icon"></i>
-            </span>
-            <p className="card__text">مالدیو</p>
-            <span className="">
-              <i className="fa fa-user card__icon"></i>
-            </span>
-            <p className="card__text">4 اتاق</p>
-            <span className="">
-              <i className="fa fa-expand card__icon"></i>
-            </span>
-            <p className="card__text">400 متر مربع</p>
-            <span className="">
-              <i className="fa fa-key card__icon"></i>
-            </span>
-            <p className="card__text">560 میلیون تومان</p>
-          </div>
-
-          <a href="#" className="btn btn-brown btn-card">
-            مشاهده ملک
-          </a>
-        </div>
-        <div className="card">
-          <img src="img/gal-4.jpeg" alt="House 6" className="card__img" />
-          <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
-          <span className="card__like">
-            <i className="fa fa-heart"></i>
-          </span>
-          <div className="card__details">
-            <span className="">
-              <i className="fa fa-map-marker card__icon"></i>
-            </span>
-            <p className="card__text">مالدیو</p>
-            <span className="">
-              <i className="fa fa-user card__icon"></i>
-            </span>
-            <p className="card__text">4 اتاق</p>
-            <span className="">
-              <i className="fa fa-expand card__icon"></i>
-            </span>
-            <p className="card__text">400 متر مربع</p>
-            <span className="">
-              <i className="fa fa-key card__icon"></i>
-            </span>
-            <p className="card__text">560 میلیون تومان</p>
-          </div>
-
-          <a href="#" className="btn btn-brown btn-card">
-            مشاهده ملک
-          </a>
-        </div>
-        <div className="card">
-          <img src="img/gal-5.jpeg" alt="House 6" className="card__img" />
-          <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
-          <span className="card__like">
-            <i className="fa fa-heart"></i>
-          </span>
-          <div className="card__details">
-            <span className="">
-              <i className="fa fa-map-marker card__icon"></i>
-            </span>
-            <p className="card__text">مالدیو</p>
-            <span className="">
-              <i className="fa fa-user card__icon"></i>
-            </span>
-            <p className="card__text">4 اتاق</p>
-            <span className="">
-              <i className="fa fa-expand card__icon"></i>
-            </span>
-            <p className="card__text">400 متر مربع</p>
-            <span className="">
-              <i className="fa fa-key card__icon"></i>
-            </span>
-            <p className="card__text">560 میلیون تومان</p>
-          </div>
-
-          <a href="#" className="btn btn-brown btn-card">
-            مشاهده ملک
-          </a>
-        </div>
-        <div className="card">
-          <img src="img/gal-6.jpeg" alt="House 6" className="card__img" />
-          <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
-          <span className="card__like">
-            <i className="fa fa-heart"></i>
-          </span>
-          <div className="card__details">
-            <span className="">
-              <i className="fa fa-map-marker card__icon"></i>
-            </span>
-            <p className="card__text">مالدیو</p>
-            <span className="">
-              <i className="fa fa-user card__icon"></i>
-            </span>
-            <p className="card__text">4 اتاق</p>
-            <span className="">
-              <i className="fa fa-expand card__icon"></i>
-            </span>
-            <p className="card__text">400 متر مربع</p>
-            <span className="">
-              <i className="fa fa-key card__icon"></i>
-            </span>
-            <p className="card__text">560 میلیون تومان</p>
-          </div>
-
-          <a href="#" className="btn btn-brown btn-card">
-            مشاهده ملک
-          </a>
-        </div>
+        {randomHomes(homes,4).map((home) => (
+          <Home key={home.id} {...home} />
+        ))}
       </div>
     </>
   );
